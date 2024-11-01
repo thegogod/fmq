f0cdd2835f5b71b9dca82557aa4c2f4b3e97e75b
aacebo <aacebowork@gmail.com>
2024-11-01 13:33:33 -0400

    Update queue.go

--- server/storage/queue.go
+++ server/storage/queue.go
@@ -6,19 +6,17 @@ func (self Queue[T]) Count() int {
 	return len(self)
 }
 
-func (self Queue[T]) Push(item T) Queue[T] {
+func (self *Queue[T]) Push(item T) {
 	select {
-	case self <- item:
+	case *self <- item:
 	default:
-		queue := make(chan T, len(self)*2)
+		queue := make(chan T, len(*self)*2)
 
-		for len(self) > 0 {
-			queue <- <-self
+		for len(*self) > 0 {
+			queue <- <-*self
 		}
 
-		self = queue
-		self <- item
+		*self = queue
+		*self <- item
 	}
-
-	return self
 }
